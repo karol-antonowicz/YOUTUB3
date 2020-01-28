6013531e23bcfa47f596bf26f69a932b0bbcacca
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-28 22:16:18 +0100

    rected SideBar and SideBarItem components

---
+++ src/containers/SideBar/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SideBarItem} from './SideBarItem';
+import {Menu} from 'semantic-ui-react';
+import './SideBar.scss';
+
+export const SideBar = () => {
+    return (
+        <Menu borderless vertical stackable fixed='left' className='side-nav'>
+            {/*...*/}
+        </Menu>
+    )
+}
---
+++ src/containers/SideBar/SideBarItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Icon, Menu} from 'semantic-ui-react';
+import './SideBarItem.scss'
+
+export const SideBarItem = props => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
