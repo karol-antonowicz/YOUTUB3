06884b89cb219c517bcff706afc38a3eea208875
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-29 22:35:30 +0100

    installed Enzyme and Jest for testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'; // ENZYME CONFIGURE TO CURRENT REACT VERSION
+import {createSerializer} from 'enzyme-to-json'; // NEEDED SO JEST UNDERSTANDS ENZYME
+
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'})); // in case encountering enzyme wrapper there is some additional serializer so it works anyway
+
+// React 16 Enzyme adapter
+Enzyme.configure({adapter: new Adapter()});
