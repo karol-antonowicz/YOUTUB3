56c5f3272a5c81c2795a450e7103fba8a1ff289b
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-29 20:54:37 +0100

    Subscription component will have an image of subscribed channel, name of the channel and info if the channel is currently broadcasting or has number of new videos. name of the cnanel, briadcasting and newvideos are passed thru props

---
+++ src/containers/SideBar/Subscriptions/Subscription/Subscription.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Icon, Image, Menu} from 'semantic-ui-react';
+import './Subscription.scss';
+
+export const Subscription = props => {
+    let rightElement = null;    // if there is no new videos, and no broadcasting then nothing is there, otherwise it wud throw an error of undefined I guess
+    const {broadcasting, amountNewVideos} = props;
+    if (broadcasting) {
+        let rightElement = <Icon name='signal'/>
+    } else if (amountNewVideos) {
+        let rightElement = <span className='new-videos-count'>{amountNewVideos}</span>
+    }
+
+    return (
+        <Menu.Item>
+            <div className='subscription'>
+                <div>
+                    <Image src='http://via.placeholder.com/28x28' avatar/>
+                    <span>{props.label}</span>
+                </div>
+                {rightElement}
+            </div>
+        </Menu.Item>
+
+    )
+}
