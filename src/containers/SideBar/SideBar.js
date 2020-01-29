48d6453b7680280973df1ff8a3bf0d68dbb8b7d6
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-29 20:38:23 +0100

    added headers to sidebar + some dividers. Craeted Subscriptions and Subscription componenets, where users subscribed channels will show up

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SideBarItem} from './SideBarItem';
-import {Menu} from 'semantic-ui-react';
+import {SideBarHeader} from './SideBarHeader';
+import {Menu, Divider} from 'semantic-ui-react';
 import './SideBar.scss';
 
 export const SideBar = () => {
@@ -9,13 +10,19 @@ export const SideBar = () => {
             <SideBarItem highlight={true} label='Home' icon='home'/>
             <SideBarItem label='Trending' icon='fire'/>
             <SideBarItem label='Subscriptions' icon='inbox'/>
+            <Divider/>
+            <SideBarHeader title='library'/>
             <SideBarItem label='History' icon='time'/>
             <SideBarItem label='Watch Later' icon='time'/>
             <SideBarItem label='Liked videos' icon='thumbs up'/>
+            <Divider/>
+            <SideBarHeader title='more from youtube'/>
             <SideBarItem label='Movies and Shows' icon='video'/>
+            <Divider/>
             <SideBarItem label='Settings' icon='setting'/>
             <SideBarItem label='Help' icon='help'/>
             <SideBarItem label='Send feedback' icon='chat outline'/>
+            <Divider/>
         </Menu>
     )
 }
