f973d0ffeccb40ab8d442cd4d5c47a1061dc59b4
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-30 14:18:42 +0100

    added more tests for SideBarItem with props checking

--- src/containers/SideBar/__tests__/SideBarItem.unit.test.js
+++ src/containers/SideBar/__tests__/SideBarItem.unit.test.js
@@ -2,9 +2,30 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {SideBarItem} from '../SideBarItem';
 
+
+describe('SideBarItem', () => {
+
 test('renders SideBarItem', ()=>{
     const wrapper = shallow(
         <SideBarItem/>
     );
     expect(wrapper).toMatchSnapshot();
 });
+
+test('renders highlighed SideBarItem that navigates to /feed/trending', ()=>{
+    const wrapper = shallow(
+        <SideBarItem highlight icon='fire' label='Trending'/>
+    );
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('renders non-highlight SideBarItem that navigates to /feed/trending', ()=>{
+    const wrapper = shallow(
+        <SideBarItem icon='fire' label='Trending'/>
+    )
+    expect(wrapper).toMatchSnapshot();
+});
+
+
+}
+)
