845dcece260f3a28f5f9352e2da8103561524275
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-30 14:29:41 +0100

    added snapshot tests for SideBarHeader and Subscription

--- src/containers/SideBar/Subscriptions/Subscription/__tests__/Subscription.unit.test.js
+++ src/containers/SideBar/Subscriptions/Subscription/__tests__/Subscription.unit.test.js
@@ -2,9 +2,41 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {Subscription} from '../Subscription';
 
-test('renders Subscription', ()=>{
+describe('Subscription', () => {
+
+test('renders Subscription with props=null', ()=>{
     const wrapper = shallow(
         <Subscription/>
     );
     expect(wrapper).toMatchSnapshot();
 });
+
+test('renders Subscription broadcasting, and no new videos', ()=>{
+    const wrapper = shallow(
+        <Subscription broadcasting label='sample'/>
+    );
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('renders Subscription broadcasting, and some new videos', ()=>{
+    const wrapper = shallow(
+        <Subscription broadcasting amountNewVideo={69} label='sample'/>
+    );
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('renders Subscription not broadcasting, and some new videos', ()=>{
+    const wrapper = shallow(
+        <Subscription amountNewVideo={69} label='sampledupa'/>
+    );
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('renders Subscription broadcasting, and no label no new videos', ()=>{
+    const wrapper = shallow(
+        <Subscription broadcasting/>
+    );
+    expect(wrapper).toMatchSnapshot();
+});
+
+})
--- src/containers/SideBar/__tests__/SideBarHeader.unit.test.js
+++ src/containers/SideBar/__tests__/SideBarHeader.unit.test.js
@@ -2,9 +2,28 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {SideBarHeader} from '../SideBarHeader';
 
-test('renders SideBarHeader', ()=>{
+
+describe('SideBarHeader', () =>{ 
+
+test('renders SideBarHeader with props.title=null', ()=>{
     const wrapper = shallow(
         <SideBarHeader/>
     );
     expect(wrapper).toMatchSnapshot();
 });
+
+test('renders SideBarHeader with props.title=\'\' ', ()=> {
+    const wrapper = shallow (
+        <SideBarHeader title=''/>
+    )
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('renders SideBarHeader with props.title=\'dupa\' ', ()=> {
+    const wrapper = shallow (
+        <SideBarHeader title='dupa'/>
+    )
+    expect(wrapper).toMatchSnapshot();
+})
+
+})
