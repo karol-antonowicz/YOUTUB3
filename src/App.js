5c586f082a949ab36922bf6686fe7b94810ed312
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-30 15:59:03 +0100

    created Home and VideoPreview component, added some margins to Home component so it shows up on the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderNav from './containers/HeaderNav/HeaderNav'
 import { SideBar } from './containers/SideBar/SideBar';
+import { Home } from './components/Home/Home';
 
 const App = () => {
   return (
     <React.Fragment>
 <HeaderNav/>
 <SideBar/>
+<Home/>
     </React.Fragment>
   );
 }
---
+++ src/components/VideoPreview/VideoPreview.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './VideoPreview.scss'
+
+export const VideoPreview = props => {
+    return (
+        <p>VideoPreview</p>
+    )
+}
