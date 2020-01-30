c7ef7fbf61a5ce429f6c53207843726514b737fe
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-30 18:02:51 +0100

    stylin VideoPreview compononet

--- src/components/VideoPreview/VideoPreview.js
+++ src/components/VideoPreview/VideoPreview.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Image } from 'semantic-ui-react'
 import './VideoPreview.scss'
 
 export const VideoPreview = props => {
     return (
-        <p>VideoPreview</p>
-    )
+        <div className='video-preview'>
+          <div className='image-container'>
+            <Image src='http://via.placeholder.com/210x118'/>
+            <div className='time-label'>
+              <span>05:22</span>
+            </div>
+          </div>
+        </div>
+      );
+  
 }
