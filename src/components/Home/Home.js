aa4983d7e057b02e1cc5bb05d315a35c96a6b854
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-02-03 18:42:41 +0100

    Addded VideoGrid components with styling, they are rendered in Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {VideoPreview} from '../VideoPreview/VideoPreview';
+import { VideoGrid } from '../VideoGrid/VideoGrid';
+import { SideBar } from '../../containers/SideBar/SideBar'
 import './Home.scss';
 
 export const Home = () => {
     return (
-        <div className='home'>
-        <VideoPreview/>
-        </div>
+        <React.Fragment>
+            <SideBar />
+            <div className='home'>
+                <div className='responsive-video-grid-container'>
+                    <VideoGrid title='Trending' />
+                    <VideoGrid title='Autos & Vehicles' hideDivider={true} />
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
