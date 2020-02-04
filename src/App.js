8c42e91b91a543f4a4399ab8472240cf35699a3a
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-02-04 15:43:53 +0100

    added Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Home } from './components/Home/Home';
 import { AppLayout } from './components/AppLayout/AppLayout';
+import { Watch } from './containers/Watch/Watch';
+import {Route, Switch} from 'react-router-dom'
 
 const App = () => {
   return (
     <AppLayout>
-      <Home />
+      <Switch>
+        <Route path='/watch' component={Watch}/>
+        <Route path='/' component={Home}/>
+      </Switch>
     </AppLayout>
   );
 }
